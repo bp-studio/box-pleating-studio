048a75ed6bd9847276066e2e7358b6147eb1d090
MuTsunTsai <don.m.t.tsai@gmail.com>
2021-01-13 13:09:50 +0800

    Fix Safari compatibility

--- .vscode/tweak.js
+++ .vscode/tweak.js
@@ -3,11 +3,7 @@ const through = require("through2");
 
 function tweak(t, filename) {
 	return through.obj(function transform(file, encoding, callback) {
-		if(file.isNull()) return callback(null, file);
-		if(file.isStream()) {
-			console.log('Cannot use streamed files');
-			return callback();
-		}
+		if(file.isNull() || file.isStream()) return callback(null, file);
 		if(!t || filename !== undefined && file.basename != filename) return callback(null, file);
 
 		let content = file.contents.toString(encoding || 'utf8');
