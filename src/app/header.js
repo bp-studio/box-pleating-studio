e8ca664e978f029aa3d5d9476af8ab201385a8a3
MuTsunTsai <don.m.t.tsai@gmail.com>
2021-01-14 17:23:30 +0800

    Fix minor Safari bug

--- src/app/header.js
+++ src/app/header.js
@@ -41,13 +41,12 @@ function callService(data) {
 	return new Promise((resolve, reject) => {
 		if('serviceWorker' in navigator) {
 			navigator.serviceWorker.getRegistration('/').then(reg => {
+				if(!reg.active) reject(); // Safari 在第一次執行的時候可能會進到這裡
 				let channel = new MessageChannel();
 				channel.port1.onmessage = event => resolve(event.data);
 				reg.active.postMessage(data, [channel.port2]);
-			}, reason => reject(reason));
-		} else {
-			reject("Service worker unavailable.");
-		}
+			}, reason => reject());
+		} else reject();
 	});
 }
 if('serviceWorker' in navigator) navigator.serviceWorker.addEventListener('message', event => {
