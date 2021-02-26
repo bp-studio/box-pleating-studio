f3d897e871452f569ad95f3054683677f6fc22ab
MuTsunTsai <don.m.t.tsai@gmail.com>
2021-02-26 17:32:28 +0800

    Fix version info scrolling on iPhone

--- src/worker/paper-master.ts
+++ src/worker/paper-master.ts
@@ -4,36 +4,7 @@
 if(typeof (Worker) == "undefined") {
 
 	// 舊版的 Safari 沒辦法在 Worker 裡面開 Worker，所以 master 得自己負責同樣的工作
-	(function() {
-
-		// 載入並啟動 paper.js
-		importScripts("paper-core.min.js");
-
-		let scope = new paper.PaperScope();
-		scope.setup([128, 128]); // 這個尺寸隨便都可以，不影響使用
-		let project = scope.project;
-
-		// 關掉自動更新，因為實際上這個 Worker 完全沒有要進行繪製
-		project.view.autoUpdate = false;
-
-		onmessage = function(event: MessageEvent) {
-			if(event.ports[0]) {
-				let [j1, j2] = event.data;
-
-				// 把傳入的資料還原成物件
-				let i1 = project.importJSON(j1) as paper.PathItem;
-				let i2 = project.importJSON(j2) as paper.PathItem;
-
-				// 計算交集
-				let result = i1.intersect(i2) as paper.Path;
-
-				// 傳回資料和面積（在這邊先計算好以節省主執行緒的計算量）
-				let data = [result.exportJSON(), result.area];
-				event.ports[0].postMessage(data);
-			}
-		};
-
-	})();
+	importScripts("paper-worker.js");
 
 } else {
 
