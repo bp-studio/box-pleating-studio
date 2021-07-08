1e8e1168dda51699f8e57a925d73c2722192c96d
MuTsunTsai <don.m.t.tsai@gmail.com>
2021-07-08 17:24:10 +0800

    Improve PWA installing UX

--- src/app/components/welcome.vue
+++ src/app/components/welcome.vue
@@ -15,21 +15,24 @@
 					</i18n>
 				</div>
 				<div class="browser-only col-12 col-lg-10 col-xl-8">
-					<div v-if="(bi||ios)&&!install">
+					<div v-if="(preparing||bi||ios)&&!install">
 						<p v-t="'welcome.install.hint'"></p>
 						<p v-if="ios" v-t="'welcome.install.ios'"></p>
-						<button v-else class="btn btn-primary" @click="bi.prompt()" v-t="'welcome.install.bt'"></button>
+						<button v-else-if="bi" class="btn btn-primary" @click="bi.prompt()" v-t="'welcome.install.bt'"></button>
+						<button v-else class="btn btn-primary" disabled>
+							{{$t('welcome.install.prepare')}}
+							<i class="bp-spinner fa-spin"></i>
+						</button>
 					</div>
-					<div v-if="install==1" v-t="'welcome.install.ing'"></div>
+					<div v-if="install==1">
+						<button class="btn btn-primary" disabled>
+							{{$t('welcome.install.ing')}}
+							<i class="bp-spinner fa-spin"></i>
+						</button>
+					</div>
 					<div v-if="install==2">
 						<p v-t="'welcome.install.ed'"></p>
-						<a
-							class="btn btn-primary"
-							rel="noopener"
-							href="https://bpstudio.abstreamace.com/"
-							target="_blank"
-							v-t="'welcome.install.open'"
-						></a>
+						<a class="btn btn-primary" rel="noopener" :href="origin" target="_blank" v-t="'welcome.install.open'"></a>
 					</div>
 				</div>
 			</div>
@@ -77,6 +80,7 @@
 
 	@Component
 	export default class Welcome extends Vue {
+		protected preparing: boolean = false;
 		protected bi: BeforeInstallPromptEvent;
 		protected install: number = 0;
 		protected ios: boolean = navigator.standalone === false;
@@ -86,6 +90,14 @@
 
 		created(): void {
 			const APP_CHECK_INTERVAL = 2000;
+
+			// 如果啟動的瞬間沒有 SW 存在，就表示 SW 正在安裝
+			// 此時顯示等候訊息以改善 UX，因為 beforeinstallprompt 事件必須等到 SW 裝完才會觸發
+			if('onbeforeinstallprompt' in window && !navigator.serviceWorker.controller) {
+				this.preparing = true;
+			}
+
+			// 監聽事件
 			window.addEventListener("beforeinstallprompt", (event: BeforeInstallPromptEvent) => {
 				event.preventDefault();
 				this.bi = event;
@@ -98,15 +110,24 @@
 					else clearInterval(i);
 				}, APP_CHECK_INTERVAL);
 			});
+
+			// 立刻檢查安裝狀態
 			this.detectInstallation();
 		}
 
 		private detectInstallation(): void {
 			if('getInstalledRelatedApps' in navigator) {
 				navigator.getInstalledRelatedApps!().then(apps => {
+					// 請注意這段程式碼只有在 Android 上面有效，
+					// 桌機上面會傳回空陣列，所以無法以此法偵測 PWA 是否已安裝；
+					// 不過這無所謂，因為開啟 PWA 的連結本來也就只有在 Android 中有效
 					if(apps.length) this.install = 2;
 				});
 			}
+		}
+
+		protected get origin(): string {
+			return location.origin;
 		}
 
 		protected get recent(): readonly FileSystemFileHandle[] {
