7da918ec2c605fe5f86fef4fb65ece0b2dd07419
Mu-Tsun Tsai <don.m.t.tsai@gmail.com>
2023-02-14 15:59:50 +0800

    Fix menu title i18n

--- src/app/vue/gadgets/menu/dropdown.vue
+++ src/app/vue/gadgets/menu/dropdown.vue
@@ -1,12 +1,13 @@
 <template>
 	<div class="btn-group" ref="el" @mouseenter.once="init" @touchstart.once.passive="init">
-		<button ref="btn" type="button" @mouseenter="mouseenter" :title="title" :disabled="!Studio.initialized"
+		<button ref="btn" type="button" @mouseenter="mouseenter" :title="initialized ? title : ''" :disabled="!Studio.initialized"
 				class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
 			<i :class="icon" />
 			<div class="notify" v-if="notify"></div>
 		</button>
 		<!--下拉選單的內容唯有等到初始化完成之後才會顯示。
-			這樣做的最大好處在於 SSG 的時候不會受到語系的影響。-->
+			這樣做的最大好處在於 SSG 的時候不會受到語系的影響。
+			上面的 title 那邊則是另外加上處理。-->
 		<div ref="menu" class="dropdown-menu" v-if="initialized">
 			<slot></slot>
 		</div>
