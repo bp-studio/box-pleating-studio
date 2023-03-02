d4d751f9ae883ad5345002f0dcc10a26247537da
Mu-Tsun Tsai <don.m.t.tsai@gmail.com>
2023-03-02 13:51:25 +0800

    PolyBool test tool

---
+++ test/tests/README.md
@@ -0,0 +1,4 @@
+
+# Tests
+
+This section contains testing tools that are not part of the Mocha unit tests.
---
+++ test/tests/polyBool/index.ts
@@ -0,0 +1,47 @@
+import { AAUnion } from "core/math/polyBool/union/aaUnion";
+
+import type { Polygon } from "shared/types/geometry";
+
+const factor = 4;
+const full = 150 * factor;
+
+const toast = document.getElementById("toast") as HTMLDivElement;
+const cInput = document.getElementById("cInput") as HTMLCanvasElement;
+const cOutput = document.getElementById("cOutput") as HTMLCanvasElement;
+const btn = document.getElementById("btn") as HTMLButtonElement;
+
+btn.onclick = run;
+
+cInput.onmousemove = e => toast.innerText = `${Math.round(e.offsetX / factor)}, ${Math.round((full - e.offsetY) / factor)}`;
+cOutput.onmousemove = e => toast.innerText = `${Math.round(e.offsetX / factor)}, ${Math.round((full - e.offsetY) / factor)}`;
+cInput.width = full;
+cInput.height = full;
+cOutput.width = full;
+cOutput.height = full;
+
+const ctxInput = cInput.getContext("2d")!;
+const ctxOutput = cOutput.getContext("2d")!;
+
+function draw(ctx: CanvasRenderingContext2D, ...polygons: Polygon[]): void {
+	ctx.clearRect(0, 0, full, full);
+	for(const polygon of polygons) {
+		for(const path of polygon) {
+			ctx.beginPath();
+			const last = path[path.length - 1];
+			ctx.moveTo(last.x * factor + 0.5, full - last.y * factor + 0.5);
+			for(const p of path) ctx.lineTo(p.x * factor + 0.5, full - p.y * factor + 0.5);
+			ctx.closePath();
+			ctx.stroke();
+		}
+	}
+}
+
+function run(): void {
+	const textArea = document.getElementById("T") as HTMLTextAreaElement;
+	const data = JSON.parse(textArea.value) as Polygon[];
+	draw(ctxInput, ...data);
+	const result = new AAUnion().$get(...data);
+	draw(ctxOutput, result);
+}
+
+
