7f9678f9f86280fbe56f5860c634bef7f56e957e
Mu-Tsun Tsai <don.m.t.tsai@gmail.com>
2024-10-09 16:12:50 +0800

    Fix bug in Mountable class

--- src/client/base/mountable.ts
+++ src/client/base/mountable.ts
@@ -23,18 +23,23 @@ export abstract class Mountable extends Destructible {
 
 	/**
 	 * Whether parent is `null` or is mounted.
+	 *
 	 * The initial value is `true`, since the parent relations
 	 * are always established after the constructors.
 	 */
 	private _parentIsNullOrMounted: boolean = true;
 
-	/** The cached value of {@link $mounted}, used for comparing changes. */
-	private _mounted: boolean;
+	/**
+	 * The cached value of {@link $mounted}, used for comparing changes.
+	 *
+	 * The initial value is `undefined`,
+	 * so that changing to either value will trigger event firing.
+	 */
+	private _mountedCache: boolean | undefined = undefined;
 
 	constructor(active: boolean = true) {
 		super();
 		this._active = active;
-		this._mounted = this.$mounted;
 
 		/**
 		 * {@link Mountable} will destruct all children on destructing,
@@ -96,9 +101,9 @@ export abstract class Mountable extends Destructible {
 	private _updateMountedState(): void {
 		if(this.$destructed) return;
 		const mounted = this.$mounted;
-		if(this._mounted == mounted) return;
+		if(this._mountedCache === mounted) return;
 		this.dispatchEvent(new StateChangedEvent(MOUNTED, mounted));
-		this._mounted = mounted;
+		this._mountedCache = mounted;
 	}
 }
 
