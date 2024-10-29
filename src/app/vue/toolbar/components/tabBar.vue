8ec7b43784874310748b8fc94b6578f116ca83e9
Mu-Tsun Tsai <don.m.t.tsai@gmail.com>
2024-10-29 12:58:07 +0800

    Fix tab scrolling on mobile devices

--- src/app/vue/toolbar/components/tabBar.vue
+++ src/app/vue/toolbar/components/tabBar.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="flex-grow-1 tab-container" @wheel.passive="tabWheel($event)" role="tablist">
-		<SlickList lockAxis="x" axis="x" v-model:list="Workspace.ids.value" :distance="10" id="divTab"
+		<SlickList lockAxis="x" axis="x" v-model:list="Workspace.ids.value" v-bind="slickAttrs()" id="divTab"
 				   :class="{ 'hide': !Workspace.ids.value.length }" @sort-start="setCursor('ew-resize')"
 				   @sort-end="setCursor('default')">
 			<SlickItem v-for="(id, i) in Workspace.ids.value" :key="id" :index="i" class="tab"
@@ -15,19 +15,42 @@
 <script setup lang="ts">
 
 	import { SlickList, SlickItem } from "vue-slicksort";
+	import { onMounted, shallowRef } from "vue";
 
 	import { compRef } from "app/utils/compRef";
 	import Workspace from "app/services/workspaceService";
 	import Studio from "app/services/studioService";
 	import Tab from "./tab.vue";
 	import TabMenu from "./tabMenu.vue";
+	import { isTouch } from "app/shared/constants";
 
 	import type { ProjId } from "shared/json";
 
 	defineOptions({ name: "TabBar" });
 
 	const tabMenu = compRef(TabMenu);
+	const hasScrollbar = shallowRef(false);
 	const DELTA_UNIT = 5;
+
+	onMounted(() => {
+		if(!isTouch) return;
+
+		// Listening to scrollbar changes for touch devices
+		const el = document.querySelector("#divTab")!;
+		const update = (): boolean => hasScrollbar.value = el.scrollWidth > el.clientWidth;
+		const observer = new MutationObserver(update);
+		window.addEventListener("resize", update);
+		observer.observe(el, { childList: true, subtree: true });
+		update();
+	});
+
+	function slickAttrs(): object {
+		if(!isTouch || !hasScrollbar.value) return { distance: 10 };
+
+		// For touch devices and only when there is scrollbar,
+		// we use the pressDelay mode so that scrolling is prioritized.
+		return { pressDelay: 300, pressThreshold: 10 };
+	}
 
 	function tabWheel(event: WheelEvent): void {
 		if(event.deltaX == 0) {
