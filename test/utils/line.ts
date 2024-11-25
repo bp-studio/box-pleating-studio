9556ad8f66fdda54b6149fc6b980e54bc43d58c4
Mu-Tsun Tsai <don.m.t.tsai@gmail.com>
2024-11-25 09:38:42 +0800

    Fix tests related to CPLine

--- test/utils/line.ts
+++ test/utils/line.ts
@@ -2,6 +2,7 @@ import { Line } from "core/math/geometry/line";
 import { pointToString } from "core/math/geometry/path";
 import { same } from "shared/types/geometry";
 
+import type { CPLine, CreaseType } from "shared/types/cp";
 import type { ILine } from "shared/types/geometry";
 
 declare global {
@@ -31,4 +32,12 @@ Assertion.addMethod("containLine", function(line: ILine) {
 export function parseLine(line: string) {
 	const m = line.match(/-?\d+/g)!.map(n => Number(n));
 	return Line.$fromIPoint({ x: m[0], y: m[1] }, { x: m[2], y: m[3] });
+}
+
+export function makeCPLine(type: CreaseType, x1: number, y1: number, x2: number, y2: number): CPLine {
+	return {
+		type,
+		p1: { x: x1, y: y1 },
+		p2: { x: x2, y: y2 },
+	};
 }
