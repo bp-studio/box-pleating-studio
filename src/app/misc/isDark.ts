eb0be20e6601be99fcdcb253ccc00ed603b6bbb6
Mu-Tsun Tsai <don.m.t.tsai@gmail.com>
2025-03-26 17:26:27 +0800

    Refactor `isDark` module

--- src/app/misc/isDark.ts
+++ src/app/misc/isDark.ts
@@ -6,11 +6,19 @@ const mm = matchMedia("(prefers-color-scheme: dark)");
 const isPreferred = shallowRef(mm.matches);
 mm.onchange = () => isPreferred.value = mm.matches;
 
-export const isPrinting = shallowRef(false);
+const forceLight = shallowRef(false);
+
+/** Run a synchronous action in light mode. */
+export function forceLightMode<T>(action: Action<T>): T {
+	forceLight.value = true;
+	const result = action();
+	forceLight.value = false;
+	return result;
+}
 
 /** If the app is currently in dark mode */
 export const isDark = computed(() => {
-	if(isPrinting.value) return false; // Always print in light mode;
+	if(forceLight.value) return false;
 	const theme = Settings.theme;
 	const result = theme == "dark" || theme == "system" && isPreferred.value;
 	document.documentElement.classList.toggle("dark", result);
