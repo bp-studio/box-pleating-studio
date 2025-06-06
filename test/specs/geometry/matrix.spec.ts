35257a9a0645fa33297b104a6b7c6031fb011c28
Mu-Tsun Tsai <don.m.t.tsai@gmail.com>
2025-06-07 00:33:39 +0800

    Refactor tests

--- test/specs/geometry/matrix.spec.ts
+++ test/specs/geometry/matrix.spec.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 
+import { Fraction } from "core/math/fraction";
 import { Matrix } from "core/math/geometry/matrix";
 import { Vector } from "core/math/geometry/vector";
 import { applyTransform } from "shared/types/geometry";
@@ -17,6 +18,12 @@ export default function() {
 
 		const transformed = applyTransform(p1, [...matrix.toArray(), 0, 0]);
 		expect(transformed).to.eql(p2);
+	});
+
+	it("Computes inverse", function() {
+		const m = new Matrix(new Fraction(1), new Fraction(2), new Fraction(3), new Fraction(4));
+		const inv = m.$inverse!;
+		expect(inv.toString()).to.equal("-2,1,3/2,-1/2");
 	});
 
 }
