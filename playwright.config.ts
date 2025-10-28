b1081db1dc60ae134d25bc924840efdac6310cad
Mu-Tsun Tsai <don.m.t.tsai@gmail.com>
2025-10-28 10:58:11 +0800

    Update dependencies

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,8 +15,8 @@ export default defineConfig({
 	fullyParallel: true,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	forbidOnly: Boolean(process.env.CI),
-	/* Retry on CI only */
-	retries: process.env.CI ? 2 : 0,
+	/* Retry once on failure */
+	retries: 1,
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
