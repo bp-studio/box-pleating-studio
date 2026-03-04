fc83dc9d5f0104701cf290bc5143f20462a0ef8e
Mu-Tsun Tsai <don.m.t.tsai@gmail.com>
2026-03-04 10:11:38 +0800

    Fix typing

--- src/app/services/errorReportService.ts
+++ src/app/services/errorReportService.ts
@@ -8,16 +8,6 @@ const JSON_INDENT = 4;
 /** @see [error.php](../../other/php/error.php) */
 const WEBHOOK = "https://abstreamace.com/php-tool/bpstudio/error.php";
 
-declare global {
-	interface Navigator {
-		/**
-		 * @see https://developer.mozilla.org/en-US/docs/Web/API/Navigator/deviceMemory
-		 */
-		// eslint-disable-next-line @typescript-eslint/no-magic-numbers
-		readonly deviceMemory?: 0.25 | 0.5 | 1 | 2 | 4 | 8;
-	}
-}
-
 /**
  * Submit the fatal error report to Discord webhook.
  *
---
+++ src/shared/frontend/navigator.d.ts
@@ -0,0 +1,7 @@
+interface Navigator {
+	/**
+	 * @see https://developer.mozilla.org/en-US/docs/Web/API/Navigator/deviceMemory
+	 */
+	// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+	readonly deviceMemory?: 0.25 | 0.5 | 1 | 2 | 4 | 8;
+}
