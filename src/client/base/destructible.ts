bp-studio/box-pleating-studio

commit fde4207a061d40b743297afce573d60f22377e8a

Eliminate decorators

src/client/shared/decorators.ts
@@ -1,82 +1,39 @@
 import { shallowRef } from "vue";
 
-import type HistoryManager from "client/project/changes/history";
 import type { ShallowRef } from "vue";
-import type { ITagObject } from "./interface";
+import type { Project } from "client/project/project";
 
-const REF_MAP = Symbol("RefMap");
-
-/**
- * A symbol that represents uninitialized {@link field}.
- * It is possible for a field value to be `undefined`,
- * so we use this symbol for that purpose.
- */
-const INIT = Symbol("uninitialized");
-
-interface RefMapTarget {
-	[REF_MAP]: Record<string, ShallowRef<unknown>>;
-}
-
-function initMap<V>(target: unknown, name: string, v: V): void {
-	((target as RefMapTarget)[REF_MAP] ||= {})[name] = shallowRef(v);
-}
-
-function getRef<V>(target: unknown, name: string): ShallowRef<V> {
-	return (target as RefMapTarget)[REF_MAP][name] as ShallowRef<V>;
+interface ITagObject {
+	readonly $tag: string;
+	readonly $project: Project;
 }
 
 /**
- * Use {@link shallowRef} in Vue to implement an accessor property.
- */
-function shallowRefDecorator<This, V>(
-	target: ClassAccessorDecoratorTarget<This, V>,
-	context: ClassAccessorDecoratorContext<This, V>
-): ClassAccessorDecoratorResult<This, V> {
-	const name = context.name as string;
-	return {
-		set(v: V) {
-			getRef(this, name).value = v;
-		},
-		get() {
-			return getRef(this, name).value as V;
-		},
-		init(v: V) {
-			initMap(this, name, v);
-			return v;
-		},
-	};
-}
-
-export { shallowRefDecorator as shallowRef };
-
-/**
- * Besides {@link shallowRefDecorator shallowRef}, implement {@link HistoryManager.$fieldChange} on this accessor property.
+ * {@link Field} wraps a {@link shallowRef} and additionally
+ * calls {@link history.$fieldChange} on set, implementing undo/redo support.
  *
  * This only works for purely synchronous properties.
- * For asynchronous properties, one must implement {@link HistoryManager.$fieldChange} elsewhere.
+ * For asynchronous properties, one must implement {@link history.$fieldChange} elsewhere.
  */
-export function field<This extends ITagObject, V>(
-	target: ClassAccessorDecoratorTarget<This, V>,
-	context: ClassAccessorDecoratorContext<This, V>
-): ClassAccessorDecoratorResult<This, V> {
-	const name = context.name as string;
-	return {
-		set(v: V) {
-			const ref = getRef(this, name);
-			const oldValue = ref.value;
-			if(oldValue === v) return;
-			ref.value = v;
-			if(oldValue !== INIT) {
-				// Signify changes only after the field is initialized.
-				this.$project.history.$fieldChange(this, name, oldValue, v);
-			}
-		},
-		get() {
-			return getRef(this, name).value as V;
-		},
-		init(v: V) {
-			initMap(this, name, v === undefined ? INIT : v);
-			return v;
-		},
-	};
+export class Field<V> {
+	private readonly _ref: ShallowRef<V>;
+	private readonly _target: ITagObject;
+	private readonly _prop: string;
+
+	constructor(target: ITagObject, prop: string, initial: V) {
+		this._target = target;
+		this._prop = prop;
+		this._ref = shallowRef<V>(initial);
+	}
+
+	public get value(): V {
+		return this._ref.value as V;
+	}
+
+	public set value(v: V) {
+		const oldValue = this._ref.value;
+		if(oldValue === v) return;
+		this._ref.value = v;
+		this._target.$project.history.$fieldChange(this._target, this._prop, oldValue, v);
+	}
 }

pnpm-lock.yaml
@@ -4048,8 +4048,8 @@ packages:
     resolution: {integrity: sha512-Hs59xBNfUIunMFgWAbGX5cq6893IbWg4KnrjbYwX3tx0ztorVgTDA6B2sxf8ejHJ4wz8BqGUMYlnzNBer5NvGg==}
     engines: {node: '>= 4'}
 
-  immutable@5.1.4:
-    resolution: {integrity: sha512-p6u1bG3YSnINT5RQmx/yRZBpenIl30kVxkTLDyHLIMk0gict704Q9n+thfDI7lTRm9vXdDYutVzXhzcThxTnXA==}
+  immutable@5.1.5:
+    resolution: {integrity: sha512-t7xcm2siw+hlUM68I+UEOK+z84RzmN59as9DZ7P1l0994DKUWV7UXBMQZVxaoMSRQ+PBZbHCOoBt7a2wxOMt+A==}
 
   import-fresh@3.3.1:
     resolution: {integrity: sha512-TR3KfrTZTYLPB6jUjfx6MF9WcWrHL9su5TObK4ZkYgBdWKPOFoSoQIdEuTuR82pmtxH2spWG9h6etwfr1pLBqQ==}
@@ -5944,8 +5944,8 @@ packages:
     resolution: {integrity: sha512-G13vtMYPT/J8A4X2SjdtBTphZlrp1gKv6hZiOjw14RCWg6GbHuQBGtjlx75xLbYV/wEc0D7G5K4rxKP/cXk8Bw==}
     engines: {node: '>=10'}
 
-  terser-webpack-plugin@5.3.16:
-    resolution: {integrity: sha512-h9oBFCWrq78NyWWVcSwZarJkZ01c2AyGrzs1crmHZO3QUg9D61Wu4NPjBy69n7JqylFF5y+CsUZYmYEIZ3mR+Q==}
+  terser-webpack-plugin@5.3.17:
+    resolution: {integrity: sha512-YR7PtUp6GMU91BgSJmlaX/rS2lGDbAF7D+Wtq7hRO+MiljNmodYvqslzCFiYVAgW+Qoaaia/QUIP4lGXufjdZw==}
     engines: {node: '>= 10.13.0'}
     peerDependencies:
       '@swc/core': '*'
@@ -10806,7 +10806,7 @@ snapshots:
 
   ignore@7.0.5: {}
 
-  immutable@5.1.4: {}
+  immutable@5.1.5: {}
 
   import-fresh@3.3.1:
     dependencies:
@@ -12352,7 +12352,7 @@ snapshots:
     dependencies:
       '@bufbuild/protobuf': 2.11.0
       colorjs.io: 0.5.2
-      immutable: 5.1.4
+      immutable: 5.1.5
       rxjs: 7.8.2
       supports-color: 8.1.1
       sync-child-process: 1.0.2
@@ -12380,7 +12380,7 @@ snapshots:
   sass@1.97.3:
     dependencies:
       chokidar: 4.0.3
-      immutable: 5.1.4
+      immutable: 5.1.5
       source-map-js: 1.2.1
     optionalDependencies:
       '@parcel/watcher': 2.5.6
@@ -12897,12 +12897,11 @@ snapshots:
       type-fest: 0.16.0
       unique-string: 2.0.0
 
-  terser-webpack-plugin@5.3.16(@swc/core@1.15.18(@swc/helpers@0.5.19))(webpack@5.102.1(@swc/core@1.15.18(@swc/helpers@0.5.19))):
+  terser-webpack-plugin@5.3.17(@swc/core@1.15.18(@swc/helpers@0.5.19))(webpack@5.102.1(@swc/core@1.15.18(@swc/helpers@0.5.19))):
     dependencies:
       '@jridgewell/trace-mapping': 0.3.31
       jest-worker: 27.5.1
       schema-utils: 4.3.3
-      serialize-javascript: 6.0.2
       terser: 5.46.0
       webpack: 5.102.1(@swc/core@1.15.18(@swc/helpers@0.5.19))
     optionalDependencies:
@@ -13333,7 +13332,7 @@ snapshots:
       neo-async: 2.6.2
       schema-utils: 4.3.3
       tapable: 2.3.0
-      terser-webpack-plugin: 5.3.16(@swc/core@1.15.18(@swc/helpers@0.5.19))(webpack@5.102.1(@swc/core@1.15.18(@swc/helpers@0.5.19)))
+      terser-webpack-plugin: 5.3.17(@swc/core@1.15.18(@swc/helpers@0.5.19))(webpack@5.102.1(@swc/core@1.15.18(@swc/helpers@0.5.19)))
       watchpack: 2.5.1
       webpack-sources: 3.3.4
     transitivePeerDependencies:

src/client/base/control.ts
@@ -1,4 +1,4 @@
-import { shallowRef } from "client/shared/decorators";
+import { shallowRef } from "vue";
 import { View } from "./view";
 import { hitMap } from "client/screen/controlEventBoundary";
 import { display } from "client/screen/display";
@@ -38,8 +38,8 @@ export abstract class Control extends View implements ITagObject {
 	 */
 	public abstract readonly $priority: number;
 
-	@shallowRef private accessor _selected: boolean = false;
-	@shallowRef private accessor _hovered: boolean = false;
+	private readonly _selected = shallowRef(false);
+	private readonly _hovered = shallowRef(false);
 
 	/** The cursor style to use when the object is selected. */
 	public $selectedCursor: string = "move";
@@ -63,11 +63,11 @@ export abstract class Control extends View implements ITagObject {
 	 * The only exceptions are the "goToDual" methods.
 	 */
 	public get $selected(): boolean {
-		return this._selected;
+		return this._selected.value;
 	}
 	public set $selected(v: boolean) {
 		if(this._hitObject) this._hitObject.cursor = v ? this.$selectedCursor : "pointer";
-		this._selected = v;
+		this._selected.value = v;
 	}
 
 	/** Whether self can be selected together with another {@link Control}. */
@@ -90,12 +90,12 @@ export abstract class Control extends View implements ITagObject {
 		object.eventMode = "static";
 		object.cursor = "pointer";
 		if(hitArea) object.hitArea = hitArea;
-		object.on("mouseenter", () => this._hovered = true);
-		object.on("mouseleave", () => this._hovered = false);
+		object.on("mouseenter", () => this._hovered.value = true);
+		object.on("mouseleave", () => this._hovered.value = false);
 		object.on("mousedown", () => display.canvas.style.cursor = this.$selectedCursor);
 	}
 
 	protected get $hovered(): boolean {
-		return this._hovered;
+		return this._hovered.value;
 	}
 }

src/client/controllers/scrollController.ts
@@ -38,8 +38,8 @@ export namespace ScrollController {
 	export function $init(): void {
 		display.scrollView.$onScroll(p => {
 			if(_scrolling) return;
-			const sheet = ProjectService.sheet.value!;
-			sheet.$scroll = p;
+			const sheet = ProjectService.sheet.value;
+			if(sheet) sheet.$scroll = p;
 		});
 
 		// Restore the scroll position as we switch the view

src/client/project/changes/commands/moveCommand.ts
@@ -24,7 +24,7 @@ export class MoveCommand extends Command implements JMoveCommand {
 		return new MoveCommand(target.$project, {
 			tag: target.$tag,
 			old: oldLocation || clone(target.$location),
-			new: newLocation,
+			new: clone(newLocation),
 		});
 	}
 
@@ -44,12 +44,14 @@ export class MoveCommand extends Command implements JMoveCommand {
 	}
 
 	public $canAddTo(command: Command): boolean {
-		return command instanceof MoveCommand && command.tag == this.tag &&
+		const result = command instanceof MoveCommand && command.tag == this.tag &&
 			// Candidate command must be immediately after self
 			command.new.x == this.old.x && command.new.y == this.old.y &&
 			// In case of keyboard moving, it must roughly towards the same direction.
 			// This solves the issue of returning to the origin.
 			(this._project.$isDragging || this._dx * command._dx >= 0 && this._dy * command._dy >= 0);
+		console.log("canAddToMove", result, this._project.$isDragging, JSON.stringify((command as MoveCommand).new), JSON.stringify(this.old), JSON.stringify(this.new));
+		return result;
 	}
 
 	public $addTo(command: Command): void {

src/client/project/changes/history.ts
@@ -1,6 +1,4 @@
-import { shallowReactive } from "vue";
-
-import { shallowRef } from "client/shared/decorators";
+import { shallowReactive, shallowRef } from "vue";
 import { FieldCommand } from "./commands/fieldCommand";
 import { Step, OperationResult, restore } from "./step";
 import { MoveCommand } from "./commands/moveCommand";
@@ -27,10 +25,10 @@ const MAX_STEP = 30;
 export default class HistoryManager implements ISerializable<JHistory> {
 
 	/** Current history location. */
-	@shallowRef private accessor _index: number = 0;
+	private readonly _index = shallowRef(0);
 
 	/** History location during last file saving. */
-	@shallowRef private accessor _savedIndex: number = 0;
+	private readonly _savedIndex = shallowRef(0);
 
 	private readonly _project: Project;
 	private readonly _steps: Step[] = shallowReactive([]);
@@ -58,21 +56,21 @@ export default class HistoryManager implements ISerializable<JHistory> {
 		if(json) {
 			try {
 				this._steps.push(...json.steps.map(s => restore(project, s)));
-				this._index = json.index;
-				this._savedIndex = json.savedIndex;
+				this._index.value = json.index;
+				this._savedIndex.value = json.savedIndex;
 			} catch {
 				// If anything goes wrong, reset everything.
 				this._steps.length = 0;
-				this._index = 0;
-				this._savedIndex = 0;
+				this._index.value = 0;
+				this._savedIndex.value = 0;
 			}
 		}
 	}
 
 	public toJSON(): JHistory {
 		return {
-			index: this._index,
-			savedIndex: this._savedIndex,
+			index: this._index.value,
+			savedIndex: this._savedIndex.value,
 			steps: this._steps.map(s => s.toJSON()),
 		};
 	}
@@ -101,11 +99,11 @@ export default class HistoryManager implements ISerializable<JHistory> {
 
 	public get isModified(): boolean {
 		// Always display as modified during dragging to reduce UI flashes.
-		return this._project.$isDragging || this._savedIndex != this._index;
+		return this._project.$isDragging || this._savedIndex.value != this._index.value;
 	}
 
 	public $notifySave(): void {
-		this._savedIndex = this._index;
+		this._savedIndex.value = this._index.value;
 	}
 
 	/** See {@link _flush}. */
@@ -155,11 +153,11 @@ export default class HistoryManager implements ISerializable<JHistory> {
 	}
 
 	public get $canUndo(): boolean {
-		return this._index > 0;
+		return this._index.value > 0;
 	}
 
 	public get $canRedo(): boolean {
-		return this._index < this._steps.length;
+		return this._index.value < this._steps.length;
 	}
 
 	public async $undo(): Promise<void> {
@@ -170,12 +168,12 @@ export default class HistoryManager implements ISerializable<JHistory> {
 		if(this.$canUndo) {
 			await display.shield(async () => {
 				this._moving = true;
-				const result = await this._steps[--this._index].$undo();
+				const result = await this._steps[--this._index.value].$undo();
 				if(result != OperationResult.Success) {
 					// If something goes wrong, the step in question and everything
 					// before that should be discarded.
-					this._steps.splice(0, this._index + (result == OperationResult.Failed ? 1 : 0));
-					this._index = 0;
+					this._steps.splice(0, this._index.value + (result == OperationResult.Failed ? 1 : 0));
+					this._index.value = 0;
 				}
 				this._flush();
 			});
@@ -190,12 +188,12 @@ export default class HistoryManager implements ISerializable<JHistory> {
 		if(this.$canRedo) {
 			await display.shield(async () => {
 				this._moving = true;
-				const result = await this._steps[this._index++].$redo();
+				const result = await this._steps[this._index.value++].$redo();
 				if(result != OperationResult.Success) {
 					// If something goes wrong, the step in question and everything
 					// after that should be discarded.
-					if(result == OperationResult.Failed) this._index--;
-					this._steps.splice(this._index);
+					if(result == OperationResult.Failed) this._index.value--;
+					this._steps.splice(this._index.value);
 				}
 				this._flush();
 			});
@@ -225,7 +223,7 @@ export default class HistoryManager implements ISerializable<JHistory> {
 				if(!step.$isVoid) this._addStep(step);
 			} else if(s.$isVoid) {
 				this._steps.pop();
-				this._index--;
+				this._index.value--;
 			}
 			this._queue = [];
 		}
@@ -249,22 +247,22 @@ export default class HistoryManager implements ISerializable<JHistory> {
 	}
 
 	private get _lastStep(): Step | undefined {
-		if(this._index == 0 || this._index < this._steps.length) return undefined;
-		return this._steps[this._index - 1];
+		if(this._index.value == 0 || this._index.value < this._steps.length) return undefined;
+		return this._steps[this._index.value - 1];
 	}
 
 	private _addStep(step: Step): void {
 		// Remove all Steps afterwards
-		if(this._steps.length > this._index) this._steps.length = this._index;
+		if(this._steps.length > this._index.value) this._steps.length = this._index.value;
 
 		// Add a new Step and move the index
-		this._steps[this._index++] = step;
+		this._steps[this._index.value++] = step;
 
 		// We keep at most 30 Steps, so get rid of the extras.
 		if(this._steps.length > MAX_STEP) {
 			this._steps.shift();
-			this._index--;
-			this._savedIndex--;
+			this._index.value--;
+			this._savedIndex.value--;
 		}
 	}
 

src/client/project/components/grid/diagonalGrid.ts
@@ -1,4 +1,4 @@
-import { shallowRef } from "client/shared/decorators";
+import { shallowRef } from "vue";
 import { GridType } from "shared/json/enum";
 import { Direction } from "shared/types/direction";
 import { drawPath } from "client/utils/contourUtil";
@@ -25,21 +25,21 @@ export class DiagonalGrid extends Grid {
 	private _testSize: number;
 	private _testShift: IPoint | undefined;
 
-	@shallowRef private accessor _size: number;
+	private readonly _size = shallowRef(0);
 
 	constructor(sheet: Sheet, width?: number, height?: number) {
 		super(sheet, GridType.diagonal);
 		width ??= DEFAULT_SIZE;
 		height ??= DEFAULT_SIZE;
-		this._testSize = this._size = Math.round((width + height) / 2);
+		this._testSize = this._size.value = Math.round((width + height) / 2);
 		if(!sheet.$project.history.$isLocked) this._initialFit();
 	}
 
 	public toJSON(): JSheet {
 		return {
 			type: this.type,
-			height: this._size,
-			width: this._size,
+			height: this._size.value,
+			width: this._size.value,
 		};
 	}
 
@@ -48,15 +48,15 @@ export class DiagonalGrid extends Grid {
 	/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	public get diameter(): number {
-		return this._size;
+		return this._size.value;
 	}
 
 	public get size(): number {
-		return this._size;
+		return this._size.value;
 	}
 	public set size(v: number) {
 		if(this.$project.history.$isLocked) {
-			this._testSize = this._size = v; // Skip all checks and effects
+			this._testSize = this._size.value = v; // Skip all checks and effects
 			return;
 		}
 
@@ -68,14 +68,14 @@ export class DiagonalGrid extends Grid {
 		if(v < oldValue && !this._testFit()) {
 			const range = Math.ceil((oldValue - v) / 2);
 			if(!this._tryShift(range)) {
-				this._testSize = this._size;
+				this._testSize = this._size.value;
 				this._testShift = undefined;
 				return;
 			}
 		}
 
 		const flush = !this._applyOffset();
-		this._size = v;
+		this._size.value = v;
 		this.$project.history.$fieldChange(this, "size", oldValue, v, flush);
 	}
 
@@ -84,7 +84,7 @@ export class DiagonalGrid extends Grid {
 	/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	public override $canSubdivide(): boolean {
-		return this._size * 2 <= MAX_SHEET_SIZE;
+		return this._size.value * 2 <= MAX_SHEET_SIZE;
 	}
 
 	public override $getResizeCenter(): IPoint {
@@ -92,13 +92,13 @@ export class DiagonalGrid extends Grid {
 	}
 
 	public override $getCenter(): IPoint {
-		return { x: this._size / 2, y: this.size / 2 };
+		return { x: this._size.value / 2, y: this.size / 2 };
 	}
 
 	public override $setDimension(width: number, height: number): void {
 		if(width != height) throw new Error("Dimension mismatch for diagonal grid.");
-		const oldValue = this._size;
-		this._testSize = this._size = width;
+		const oldValue = this._size.value;
+		this._testSize = this._size.value = width;
 		this.$project.history.$fieldChange(this, "size", oldValue, width, false);
 	}
 
@@ -108,7 +108,7 @@ export class DiagonalGrid extends Grid {
 	}
 
 	public $constrain(p: IPoint): IPoint {
-		return diagonalConstrain(this._size, this._size, p);
+		return diagonalConstrain(this._size.value, this._size.value, p);
 	}
 
 	public $contains(p: IPoint): boolean {
@@ -124,7 +124,7 @@ export class DiagonalGrid extends Grid {
 	}
 
 	public $getLabelDirection(x: number, y: number): Direction {
-		const shift = this._size % 2, s = this._size + shift, h = s / 2;
+		const shift = this._size.value % 2, s = this._size.value + shift, h = s / 2;
 		if(shift == 0) {
 			if(x == 0 && y == h) return Direction.L;
 			if(x == h && y == 0) return Direction.B;
@@ -145,7 +145,7 @@ export class DiagonalGrid extends Grid {
 
 	public $getBorderPath(): Path {
 		let full = this.$renderHeight, half = full / 2;
-		const shift = this._size % 2 / 2;
+		const shift = this._size.value % 2 / 2;
 		full -= shift;
 		half -= shift;
 		return [
@@ -157,8 +157,8 @@ export class DiagonalGrid extends Grid {
 	}
 
 	public $drawGrid(grid: GraphicsLike): void {
-		const size = this._size;
-		const shift = this._size % 2;
+		const size = this._size.value;
+		const shift = this._size.value % 2;
 		for(let i = 1 - shift; i < size + shift; i++) {
 			const offset = Math.abs(i - size / 2) - shift / 2;
 			grid.moveTo(offset, i);
@@ -171,7 +171,7 @@ export class DiagonalGrid extends Grid {
 	public $getTransformMatrix(size: number, reorient: boolean): TransformationMatrix {
 		const full = this.$renderWidth;
 		const s = size / full;
-		const shift = this._size % 2 / 2;
+		const shift = this._size.value % 2 / 2;
 		if(reorient) {
 			const offset = full - 2 * shift;
 			return [s, s, s, -s, -s * offset, 0];
@@ -182,16 +182,16 @@ export class DiagonalGrid extends Grid {
 	}
 
 	public get $offset(): IPoint {
-		const shift = this._size % 2 / 2;
+		const shift = this._size.value % 2 / 2;
 		return { x: -shift, y: -shift };
 	}
 
 	public get $renderHeight(): number {
-		return this._size + this._size % 2;
+		return this._size.value + this._size.value % 2;
 	}
 
 	public get $renderWidth(): number {
-		return this._size + this._size % 2;
+		return this._size.value + this._size.value % 2;
 	}
 
 	/////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -223,7 +223,7 @@ export class DiagonalGrid extends Grid {
 				this._testShift = { x: this._testShift.x + 1, y: this._testShift.y + 1 };
 			}
 		}
-		this._size = this._testSize;
+		this._size.value = this._testSize;
 		this._applyOffset();
 	}
 

src/client/project/components/grid/rectangularGrid.ts
@@ -1,4 +1,4 @@
-import { shallowRef } from "client/shared/decorators";
+import { shallowRef } from "vue";
 import { GridType } from "shared/json/enum";
 import { Direction } from "shared/types/direction";
 import { Grid } from "./grid";
@@ -23,20 +23,20 @@ export class RectangularGrid extends Grid {
 	private _testWidth: number;
 	private _testHeight: number;
 
-	@shallowRef private accessor _width: number;
-	@shallowRef private accessor _height: number;
+	private readonly _width = shallowRef(0);
+	private readonly _height = shallowRef(0);
 
 	constructor(sheet: Sheet, width?: number, height?: number) {
 		super(sheet, GridType.rectangular);
-		this._testHeight = this._height = height ?? DEFAULT_SIZE;
-		this._testWidth = this._width = width ?? DEFAULT_SIZE;
+		this._testHeight = this._height.value = height ?? DEFAULT_SIZE;
+		this._testWidth = this._width.value = width ?? DEFAULT_SIZE;
 	}
 
 	public toJSON(): JSheet {
 		return {
 			type: this.type,
-			height: this._height,
-			width: this._width,
+			height: this._height.value,
+			width: this._width.value,
 		};
 	}
 
@@ -45,20 +45,20 @@ export class RectangularGrid extends Grid {
 	/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	public get diameter(): number {
-		return Math.max(this._height, this._width);
+		return Math.max(this._height.value, this._width.value);
 	}
 
 	public get height(): number {
-		return this._height;
+		return this._height.value;
 	}
 	public set height(v: number) {
 		if(this.$project.history.$isLocked) {
-			this._testHeight = this._height = v;
+			this._testHeight = this._height.value = v;
 			return;
 		}
 
 		let flush = true;
-		const oldValue = this._height;
+		const oldValue = this._height.value;
 		if(v < MIN_RECT_SIZE || v > MAX_SHEET_SIZE || oldValue === v) return;
 		this._testHeight = v;
 		if(v < oldValue) {
@@ -71,21 +71,21 @@ export class RectangularGrid extends Grid {
 				flush = false;
 			}
 		}
-		this._height = v;
+		this._height.value = v;
 		this.$project.history.$fieldChange(this, "height", oldValue, v, flush);
 	}
 
 	public get width(): number {
-		return this._width;
+		return this._width.value;
 	}
 	public set width(v: number) {
 		if(this.$project.history.$isLocked) {
-			this._testWidth = this._width = v;
+			this._testWidth = this._width.value = v;
 			return;
 		}
 
 		let flush = true;
-		const oldValue = this._width;
+		const oldValue = this._width.value;
 		if(v < MIN_RECT_SIZE || v > MAX_SHEET_SIZE || oldValue === v) return;
 		this._testWidth = v;
 		if(v < oldValue) {
@@ -98,7 +98,7 @@ export class RectangularGrid extends Grid {
 				flush = false;
 			}
 		}
-		this._width = v;
+		this._width.value = v;
 		this.$project.history.$fieldChange(this, "width", oldValue, v, flush);
 	}
 
@@ -107,7 +107,7 @@ export class RectangularGrid extends Grid {
 	/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	public override $canSubdivide(): boolean {
-		return this._width * 2 <= MAX_SHEET_SIZE && this._height * 2 <= MAX_SHEET_SIZE;
+		return this._width.value * 2 <= MAX_SHEET_SIZE && this._height.value * 2 <= MAX_SHEET_SIZE;
 	}
 
 	public override $getResizeCenter(): IPoint {
@@ -115,14 +115,14 @@ export class RectangularGrid extends Grid {
 	}
 
 	public override $getCenter(): IPoint {
-		return { x: this._width / 2, y: this._height / 2 };
+		return { x: this._width.value / 2, y: this._height.value / 2 };
 	}
 
 	public override $setDimension(width: number, height: number): void {
-		const w = this._width;
-		const h = this._height;
-		this._testWidth = this._width = width;
-		this._testHeight = this._height = height;
+		const w = this._width.value;
+		const h = this._height.value;
+		this._testWidth = this._width.value = width;
+		this._testHeight = this._height.value = height;
 		this.$project.history.$fieldChange(this, "width", w, width, false);
 		this.$project.history.$fieldChange(this, "height", h, height, false);
 	}
@@ -133,7 +133,7 @@ export class RectangularGrid extends Grid {
 	}
 
 	public $constrain(p: IPoint): IPoint {
-		return rectangularConstrain(this._width, this._height, p);
+		return rectangularConstrain(this._width.value, this._height.value, p);
 	}
 
 	public $contains(p: IPoint): boolean {
@@ -143,7 +143,7 @@ export class RectangularGrid extends Grid {
 	}
 
 	public $getLabelDirection(x: number, y: number): Direction {
-		const w = this._width, h = this._height;
+		const w = this._width.value, h = this._height.value;
 		if(x == 0) {
 			if(y == 0) return Direction.LL;
 			if(y == h) return Direction.UL;
@@ -159,11 +159,11 @@ export class RectangularGrid extends Grid {
 	}
 
 	public $drawBorder(border: GraphicsLike): void {
-		border.drawRect(0, 0, this._width, this._height);
+		border.drawRect(0, 0, this._width.value, this._height.value);
 	}
 
 	public $getBorderPath(): Path {
-		const w = this._width, h = this._height;
+		const w = this._width.value, h = this._height.value;
 		return [
 			{ x: 0, y: 0 },
 			{ x: w, y: 0 },
@@ -173,18 +173,18 @@ export class RectangularGrid extends Grid {
 	}
 
 	public $drawGrid(grid: GraphicsLike): void {
-		for(let i = 1; i < this._height; i++) {
+		for(let i = 1; i < this._height.value; i++) {
 			grid.moveTo(0, i);
-			grid.lineTo(this._width, i);
+			grid.lineTo(this._width.value, i);
 		}
-		for(let i = 1; i < this._width; i++) {
+		for(let i = 1; i < this._width.value; i++) {
 			grid.moveTo(i, 0);
-			grid.lineTo(i, this._height);
+			grid.lineTo(i, this._height.value);
 		}
 	}
 
 	public $getTransformMatrix(size: number, reorient: boolean): TransformationMatrix {
-		const w = this._width, h = this._height;
+		const w = this._width.value, h = this._height.value;
 		const max = Math.max(w, h);
 		const s = size / max;
 		return [s, 0, 0, -s, -s * w / 2, s * h / 2];
@@ -193,11 +193,11 @@ export class RectangularGrid extends Grid {
 	public readonly $offset: IPoint = { x: 0, y: 0 };
 
 	public get $renderHeight(): number {
-		return this._height;
+		return this._height.value;
 	}
 
 	public get $renderWidth(): number {
-		return this._width;
+		return this._width.value;
 	}
 
 	/////////////////////////////////////////////////////////////////////////////////////////////////////

commit 1174dd96ce658de399544dea3c882812e1b52930

Fix bug introduced in fde4207a

src/client/base/destructible.ts
@@ -28,8 +28,11 @@ export abstract class Destructible extends EventTarget {
 		// Remove all object references to improve GC.
 		// We have to wait until all destructing actions are done to do this,
 		// or we'll end up in a lot of trouble.
+		// Skip Vue reactive wrappers (ShallowRef/Field), as Vue may still
+		// trigger re-renders after destruction; deleting them causes errors.
 		for(const key of Object.keys(this)) {
-			if(typeof this[key] === "object") delete this[key];
+			const val = this[key];
+			if(typeof val === "object" && !isReactiveWrapper(val)) delete this[key];
 		}
 
 		this._destructed = true;
@@ -49,6 +52,16 @@ export interface Destructible extends EventTarget {
 		type: T,
 		callback: Consumer<DestructibleEventMap[T]>,
 		options?: boolean | AddEventListenerOptions): void;
+}
+
+/**
+ * Check if the value is a Vue reactive wrapper ({@link ShallowRef})
+ * or a {@link Field} (which internally wraps a {@link ShallowRef}).
+ * These should be preserved after destruction since Vue may still
+ * access them during pending re-renders.
+ */
+function isReactiveWrapper(val: unknown): boolean {
+	return val != null && (val as { __v_isRef?: boolean }).__v_isRef === true;
 }
 
 const DESTRUCT = "destruct";

src/client/shared/decorators.ts
@@ -16,6 +16,9 @@ interface ITagObject {
  * For asynchronous properties, one must implement {@link history.$fieldChange} elsewhere.
  */
 export class Field<V> {
+	/** Marks this as a reactive wrapper so {@link Destructible} preserves it. */
+	public readonly __v_isRef = true;
+
 	private readonly _ref: ShallowRef<V>;
 	private readonly _target: ITagObject;
 	private readonly _prop: string;